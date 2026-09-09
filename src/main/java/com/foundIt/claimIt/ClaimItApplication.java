2d5b795696102a119bcaf38a7ffa8fdbfcf1ec1e
Abdulhadi.Abulzahab <abdulhadi.abulzahab@rabobank.nl>
2026-09-09 10:09:10 +0200

    Initial commit - project instantiation

---
+++ src/main/java/com/foundIt/claimIt/ClaimItApplication.java
@@ -0,0 +1,13 @@
+package com.foundIt.claimIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foundIt/claimIt/ClaimItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foundIt.claimIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
